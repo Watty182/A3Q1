ec5f09a9eef71eb036af34bbdb528ec891026a07
Watty182 <watty182@gmail.com>
2017-03-24 02:29:19 -0500

    Create table.h
    added includes, typedef for boolean, and prototypes within header
    guards.

---
+++ table.h
@@ -0,0 +1,21 @@
+#ifndef _TABLE_H
+#define _TABLE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+// defines Boolean to either false or true
+typedef enum BOOL {false, true} Boolean;
+
+//==============================================================
+// Prototypes
+//==============================================================
+
+Boolean search(const char *);
+Boolean insert(char const*);
+Boolean delete(const char *);
+char * firstItem(void);
+char * nextItem(void);
+#endif
